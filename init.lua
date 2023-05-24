289d8b1129b0c05b571bc9ff206c606fbe8055b4
Pascal Hartig <phartig@rdrei.net>
2023-05-24 11:04:37 +0100

    Smaller setup

--- init.lua
+++ init.lua
@@ -60,7 +60,7 @@ return {
     performance = {
       rtp = {
         -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
+        -- disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
       },
     },
   },
